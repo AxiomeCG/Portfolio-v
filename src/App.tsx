6cf5ee6c4c5cbd0816dfaf59b700464367465b24
Adam NAILI <adam.naili.pro@gmail.com>
2023-10-18 17:13:59 +0200

    feat: continue the pyramid thing (test on the logo not fully coherent) and implement two modes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Canvas, useFrame } from "@react-three/fiber";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
 import "./App.scss";
 import { OrbitControls, useTexture, Text, Sphere, Environment } from "@react-three/drei";
 
@@ -6,12 +6,55 @@ import React, { useEffect, useMemo, useRef } from "react";
 import fragmentShader from "./shaders/particles/fragmentShader";
 import vertexShader from "./shaders/particles/vertexShader";
 import * as THREE from "three-stdlib";
-import { Vector2 } from "three";
+import { PMREMGenerator, Vector2 } from "three";
 import { useControls } from "leva";
 import { Pyramid } from "./Pyramid";
+import { LogoShard } from "./LogoShard";
+import {state} from "./state";
+import {useSnapshot} from "valtio";
 const Scene = () => {
+
+
+  const texture = useTexture('/project2.png')
+
+  const gl = useThree(state => state.gl)
+
+  const pmremGenerator = new PMREMGenerator(gl)
+
+  const newTexture = pmremGenerator.fromEquirectangular(texture).texture
+
+  const {landing} = useControls({
+      landing: true
+  })
+
+  useEffect(() => {
+    state.mode = landing ? 'landing' : 'showcase'
+  }, [landing]);
+
+  const snapshot = useSnapshot(state)
+
   return <>
-    <Pyramid/>
+
+    {/*<LogoShard scale={0.5} rotation-x={-Math.PI / 2} position={[1, 0, 0]}/>*/}
+    <Pyramid  position={snapshot.mode === 'landing' ? [0,0,0] : [1, 0, 2]}/>
+    {(snapshot.mode === 'showcase') && <>
+      <Text font={'/ClashDisplay-Regular.woff'} scale={0.2} position={[-0.5, 0, 2]}>
+        Avatopia
+      </Text>
+
+      <Text font={'/ClashDisplay-Regular.woff'} scale={0.08} position={[-0.5, -0.15, 2]}>
+        Discover
+      </Text>
+
+    </>
+    }
+    {(snapshot.mode === 'landing') && <>
+      <Text font={'/ClashDisplay-Regular.woff'} position={[0,0,-1]}>{"Creative \n Technologist"}</Text>
+    </>
+    }
+
+    <Environment map={snapshot.mode === 'showcase' ? newTexture: undefined} blur={0.5} files={snapshot.mode === 'showcase' ? undefined: '/skybox.hdr'}/>
+
   </>
 }
 
@@ -19,11 +62,11 @@ const Scene = () => {
 function App() {
   return (
     <>
-      <Canvas camera={{position: [0, 0, 2]}}>
+      <Canvas camera={{position: [0, 0, 5], fov: 35}}>
         <color attach="background" args={["#13131c"]}/>
         <Scene/>
         <pointLight position={[0, 5, 0]} intensity={1} color="white"/>
-        <OrbitControls/>
+        <OrbitControls enabled={false}/>
 
       </Canvas>
     </>);
---
+++ src/state.js
@@ -0,0 +1,46 @@
+import {proxy} from 'valtio';
+
+export const state = proxy({
+    mode: 'showcase'
+})
+
+
+export const landingPageConfig =  {
+    transmissionSampler: false,
+    backside: true,
+    samples: 16,
+    resolution: 2048,
+    transmission: 1,
+    roughness: 0.25,
+    thickness: 3.5,
+    ior: 1.2,
+    chromaticAberration: 0.25,
+    anisotropy: 0.5,
+    distortion: 0.0,
+    distortionScale: 0.3,
+    temporalDistortion:0.5,
+    clearcoat: 1,
+    attenuationDistance: 0.5,
+    attenuationColor: '#ffffff',
+    color: '#c3c3c3',
+}
+
+export const showcaseConfig =  {
+    transmissionSampler: false,
+    backside: true,
+    samples: 16,
+    resolution: 2048,
+    transmission: 1,
+    roughness: 0.0,
+    thickness: 3.5,
+    ior: 1.5,
+    chromaticAberration: 0.06,
+    anisotropy: 0.01,
+    distortion: 0.0,
+    distortionScale: 0.3,
+    temporalDistortion:0.5,
+    clearcoat: 1,
+    attenuationDistance: 0.5,
+    attenuationColor: '#ffffff',
+    color: '#3b3b3b',
+}
